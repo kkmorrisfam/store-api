d42971dd7a8358f1691184af09f06edd9d1f27c6
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-02-26 20:35:31 -0800

    add select

--- controllers/products.js
+++ controllers/products.js
@@ -2,13 +2,13 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   
-  const products = await Product.find({}).sort('-name price')
+  const products = await Product.find({}).sort('-name price').select(name price)
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   
-  const { featured, company, name, sort } = req.query; // this will limit what the query is looking for
+  const { featured, company, name, sort, fields } = req.query; // this will limit what the query is looking for
   const queryObject = {};
 
   if (featured) {
@@ -22,13 +22,21 @@ const getAllProducts = async (req, res) => {
   }
   console.log(queryObject);
   let result = Product.find(queryObject)
-
+  
+  //sort
   if(sort) {
     const sortList = sort.split(',').join(' ') //split on comma from query string, change to space for syntax
     result = result.sort(sortList)
   } else {
     result = result.sort(createdAt)
   }
+  
+  //select
+  if(fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+  
   //set up await when we have the complete result
   const products = await result
 
