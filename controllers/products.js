c3124d9c25d8e601b77ef2e7375069163a8eea80
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-02-26 18:55:49 -0800

    hard code filter for testing

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async errors')  //use this instead of next(error) w/ package, don't have to set up try/catch/next
-//   res.status(200).json({ msg: "products testing route" });
+  const products = await Product.find({ 
+    // featured: true,
+    name: 'vase table'
+    
+  })
+  res.status(200).json({ products, nbHits: products.length,});
 };
 
 const getAllProducts = async (req, res) => {
