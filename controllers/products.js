79e5708938d633e68a965804b1bbdd4e3a2237c8
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-02-26 19:20:13 -0800

    logic for search on featured

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,25 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ 
+  const products = await Product.find({
     // featured: true,
-    name: 'vase table'
-    
-  })
-  res.status(200).json({ products, nbHits: products.length,});
+    name: "vase table",
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  //console.log(req.query)  //shows query values in console
+  const { featured } = req.query; // this will limit what the query is looking for
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
