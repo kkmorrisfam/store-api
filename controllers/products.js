c666a22c2c3daa8f6ab668c6ef2e7e1a1f893113
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-02-26 19:50:47 -0800

    add and limit search

--- controllers/products.js
+++ controllers/products.js
@@ -1,20 +1,26 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
+  const search = "a";
   const products = await Product.find({
-    // featured: true,
-    name: "vase table",
+    name: { $regex: search, $options: "i" },
   });
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   //console.log(req.query)  //shows query values in console
-  const { featured } = req.query; // this will limit what the query is looking for
+  const { featured, company, name } = req.query; // this will limit what the query is looking for
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
   }
   console.log(queryObject);
   const products = await Product.find(queryObject);
