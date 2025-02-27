6e33e488418a85b0adf9e7ee4b4a0e36d7f8b8bd
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-02-26 20:24:41 -0800

    add sort

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,14 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "a";
-  const products = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
+  
+  const products = await Product.find({}).sort('-name price')
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  //console.log(req.query)  //shows query values in console
-  const { featured, company, name } = req.query; // this will limit what the query is looking for
+  
+  const { featured, company, name, sort } = req.query; // this will limit what the query is looking for
   const queryObject = {};
 
   if (featured) {
@@ -23,7 +21,16 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
   console.log(queryObject);
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject)
+
+  if(sort) {
+    const sortList = sort.split(',').join(' ') //split on comma from query string, change to space for syntax
+    result = result.sort(sortList)
+  } else {
+    result = result.sort(createdAt)
+  }
+  //set up await when we have the complete result
+  const products = await result
 
   res.status(200).json({ products, nbHits: products.length });
 };
