fe8a5246a6a5a80c0fcbdb80303595ed4398b390
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-02-27 17:27:50 -0800

    add numeric filtering with operators

--- controllers/products.js
+++ controllers/products.js
@@ -1,17 +1,16 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product
-    .find({})
-    .sort("name")
-    .select("name price")
-    // .limit(30)
-    // .skip(5); //skips first 5 items in response
+  const products = await Product.find({ price: { $gt: 30 } }) //$gt for greater than; $lt for less than
+    .sort("price")
+    .select("name price");
+  // .limit(30)
+  // .skip(5); //skips first 5 items in response
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query; // this will limit what the query is looking for
+  const { featured, company, name, sort, fields, numericFilters } = req.query; // this will limit what the query is looking for
   const queryObject = {};
 
   if (featured) {
@@ -23,6 +22,31 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
+
+  if (numericFilters) {
+    //swap operators in response with mongoDB operators
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    ); //added '-' so I can split on this
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+    //  console.log(filters)
+  }
+
   console.log(queryObject);
   let result = Product.find(queryObject);
 
@@ -31,7 +55,7 @@ const getAllProducts = async (req, res) => {
     const sortList = sort.split(",").join(" "); //split on comma from query string, change to space for syntax
     result = result.sort(sortList);
   } else {
-    result = result.sort(createdAt);
+    result = result.sort("createdAt"); //default sort if not specified, use this field
   }
 
   //select
@@ -41,11 +65,11 @@ const getAllProducts = async (req, res) => {
   }
 
   //limit products per page
-  const page = Number(req.query.page) || 1
-  const limit = Number(req.query.limit) || 10   //default 10 products
-  const skip = (page-1) * limit;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10; //default 10 products
+  const skip = (page - 1) * limit;
 
-  result = result.skip(skip).limit(limit)
+  result = result.skip(skip).limit(limit);
 
   //set up await when we have the complete result
   const products = await result;
